77eab5542ce471633d216701ac82456f3299e1c4
Lev Ostapenko <012938576a@gmail.com>
2023-01-17 10:01:44 +0300

    added migrations

---
+++ migrations/00000001_initial.migration.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:conduit_core/conduit_core.dart';   
+
+class Migration1 extends Migration { 
+  @override
+  Future upgrade() async {
+   
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
