d5ebec19f37bf1a9232099f520abcb2954788b4c
Lev Ostapenko <012938576a@gmail.com>
2023-01-17 09:02:27 +0300

    init repo, dependencies

---
+++ bin/dart_backend.dart
@@ -0,0 +1,5 @@
+import 'package:dart_backend/dart_backend.dart' as dart_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_backend.calculate()}!');
+}
---
+++ test/dart_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_backend/dart_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
