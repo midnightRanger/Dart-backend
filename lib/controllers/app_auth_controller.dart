73600c3580a90e71c5862fb2e85b2c3a7062d70f
Lev Ostapenko <012938576a@gmail.com>
2023-01-25 21:24:57 +0300

    added sign up method

--- lib/controllers/app_auth_controller.dart
+++ lib/controllers/app_auth_controller.dart
@@ -4,7 +4,6 @@ import 'package:conduit/conduit.dart';
 
 import '../model/model_response.dart';
 import '../model/user.dart';
-import ''
 
 
 import '../utils/app_response.dart';class AppAuthController extends ResourceController {
@@ -61,4 +60,51 @@ Future<Response> signIn(@Bind.body() User user) async {
       return AppResponse.serverError(e);
     }
   }
+
+  @Operation.put()
+  Future<Response> signUp(@Bind.body() User user) async {
+    if (user.password == null || user.userName == null || user.email == null) {
+      return Response.badRequest(
+        body: ModelResponse(message: "Одно из полей: password, username, email не заполнено"),
+      );
+    }
+
+    //Генерация соли
+    final salt = generateRandomSalt(); 
+    //Генерация хэша пароля 
+    final hashPassword = generatePasswordHash(user.password!, salt);
+
+    try {
+      late final int id; 
+
+      //Создание транзакции 
+      await managedContext.transaction((transaction) async {
+        //Создание запроса для создания пользователя
+        final qCreateUser = Query<User> (transaction)
+          ..values.userName = user.userName
+          ..values.email = user.email
+          ..values.salt = salt
+          ..values.hashPassword = hashPassword;
+
+          //Добавление пользователя в БД
+          final createdUser = await qCreateUser.insert(); 
+
+          //Сохранение пользовательского ID
+          id = createdUser.id!;
+
+          //Обновление токена
+          _updateTokens(id,transaction);
+      });
+
+      //Получение пользовательских данных по ID 
+      final userData = await managedContext.fetchObjectWithID<User>(id);
+
+      return AppResponse.ok(body: userData!.backing.contents, 
+        message: 'Пользователь успешно зарегистрировался'
+      ); 
+    }
+    catch (e) {
+      return AppResponse.serverError(e); 
+    }
+  } 
 }
