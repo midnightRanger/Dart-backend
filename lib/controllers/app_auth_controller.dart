cfed585b9405006d232e91c4fb65b13ee142851c
Lev Ostapenko <012938576a@gmail.com>
2023-01-25 20:47:17 +0300

    added auth controller

---
+++ lib/controllers/app_auth_controller.dart
@@ -0,0 +1,62 @@
+import 'dart:html';
+
+import 'package:conduit/conduit.dart';
+
+import '../model/model_response.dart';
+import '../model/user.dart';
+
+class AppAuthController extends ResourceController {
+  AppAuthController(this.managedContext);
+
+  final ManagedContext managedContext;
+}
+
+@Operation.post() 
+Future<Response> signIn(@Bind.body() User user) async {
+  if(user.password == null || user.userName == null) {
+    return Response.badRequest(
+      body: ModelResponse(message: 'Поля: password и username обязательны'));
+  }
+
+  try {
+    final qFindUser = Query<User>(managedContext)..where((element) => element.userName).equalTo(user.userName)..
+    returningProperties((element) => [
+      element.id,
+      element.salt,
+      element.hashPassword],
+      );
+
+    //Получение первого элемента из списка
+    final findUser = await qFindUser.fetchOne();
+
+    if (findUser == null) {
+      throw QueryException.input("Пользователь не найден", []);
+    }
+
+    // генерация хэша пароля для дальнейшей проверки
+    final requestHashPassword = 
+      generatePasswordHash(user.password ?? ' ', findUser.salt ?? ' ');
+
+    // Проверка пароля 
+    if (requestHashPassword == findUser.hashPassword) {
+      //Обновления token пароля
+      _updateTokens(findUser.id ?? -1, managedContext); 
+
+      //Получаем данные пользователя 
+      final newUser = 
+        await managedContext.fetchObjectWithId<User>(findUser.id);
+
+      return Response.ok(ModelResponse(
+        data: newUser!.backing.contents, 
+        message: "Успешная авторизация!",
+      ));
+    }
+        else {
+          throw QueryException.input("Неверный пароль", []);
+        } 
+  }
+      
+    catch (e) {
+      return AppResponse.serverError(e);
+    }
+  }
